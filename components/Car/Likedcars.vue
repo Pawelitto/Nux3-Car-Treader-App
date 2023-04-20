785df958c63acb94a3fbca1dfd0cbe3611d766c0
Paweł Pietras <pawel.pietras144@gmail.com>
2023-04-20 13:44:35 +0200

    fucked watch not working :(

--- components/Car/Likedcars.vue
+++ components/Car/Likedcars.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { useLikedCar } from "@/store/store";
+import { storeToRefs } from "pinia";
+import { watch } from "../../.nuxt/imports";
 const likesStore = useLikedCar();
 const { cars } = useCars();
 
@@ -7,11 +9,17 @@ function filterCarsByLiked(cars, likes) {
   const likedCarIds = likes.map((car) => car.id);
   return cars.filter((car) => likedCarIds.includes(car.id));
 }
-
 const filteredCars = filterCarsByLiked(cars, likesStore.allLikes);
+
+const { allLikes } = storeToRefs(likesStore);
+watch(allLikes, () => {
+  console.log("allLikes ref changed");
+  const filteredCars = filterCarsByLiked(cars, likesStore.allLikes);
+});
 </script>
 <template>
   <div class="w-full">
+    {{ filteredCars }}
     <CarCard v-for="car in filteredCars" :key="car.id" :car="car" />
   </div>
 </template>
